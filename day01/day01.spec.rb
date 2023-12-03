53c8684341b9274f3a2aef90753738f12b7e1ba3
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-03 13:54:45 +0100

    Fix format

--- day01/day01.spec.rb
+++ day01/day01.spec.rb
@@ -1,24 +1,24 @@
-require_relative "day01"
-require "test/unit"
+require_relative 'day01'
+require 'test/unit'
 
 class TestDay01A < Test::Unit::TestCase
   def test_example_day01_A
-    result = day01A "day01/exampleData"
+    result = day01A 'day01/exampleData.txt'
     assert_equal result, 142
   end
 
   def test_puzzle_day01_A
-    result = day01A "day01/inputData"
+    result = day01A 'day01/inputData.txt'
     assert_equal result, 53_334
   end
 
   def test_example_day01_B
-    result = day01B "day01/exampleData2"
+    result = day01B 'day01/exampleData2.txt'
     assert_equal result, 281
   end
 
   def test_puzzle_day01_B
-    result = day01B "day01/inputData"
+    result = day01B 'day01/inputData.txt'
     assert_equal result, 52_834
   end
 end
--- day02/day02.spec.rb
+++ day02/day02.spec.rb
@@ -3,22 +3,22 @@ require 'test/unit'
 
 class TestDay02A < Test::Unit::TestCase
   def test_example_day02_A
-    result = day02A 'day02/exampleData'
+    result = day02A 'day02/exampleData.txt'
     assert_equal result, 8
   end
 
   def test_puzzle_day02_A
-    result = day02A 'day02/inputData'
+    result = day02A 'day02/inputData.txt'
     assert_equal result, 2105
   end
 
   def test_example_day02_B
-    result = day02B 'day02/exampleData'
+    result = day02B 'day02/exampleData.txt'
     assert_equal result, 2286
   end
 
   def test_puzzle_day02_B
-    result = day02B 'day02/inputData'
+    result = day02B 'day02/inputData.txt'
     assert_equal result, 72_422
   end
 end
