006a036820b9187145ad92bb369382970376cb1f
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-03 13:54:34 +0100

    Refactor Day03

--- day03/day03.rb
+++ day03/day03.rb
@@ -3,6 +3,27 @@
 SYM_REGEX = %r{[*\-$=+&@#/%]}
 GEAR_REGEX = /\*/
 
+def gather_parts(file, syms_regex)
+  parts = []
+  syms = []
+  File
+    .read(file)
+    .split("\n")
+    .each_with_index do |line, i|
+      line.scan(/\d+/) do |n|
+        parts << [
+          n.to_i,
+          [$LAST_MATCH_INFO.offset(0)[0], i],
+          [$LAST_MATCH_INFO.offset(0)[0] + n.length - 1, i]
+        ]
+      end
+      line.scan(syms_regex) do |s|
+        syms << [s, [$LAST_MATCH_INFO.offset(0)[0], i]]
+      end
+    end
+  [parts, syms]
+end
+
 def adjacent?(p, s)
   h = p[0] == s[0] && (p[1] - s[1]).abs == 1
   v = p[1] == s[1] && (p[0] - s[0]).abs == 1
@@ -23,23 +44,7 @@ def gear_adjacent_parts(gear, parts)
 end
 
 def day03A(file)
-  parts = []
-  syms = []
-  File
-    .read(file)
-    .split("\n")
-    .each_with_index do |line, i|
-      line.scan(/\d+/) do |n|
-        parts << [
-          n.to_i,
-          [$LAST_MATCH_INFO.offset(0)[0], i],
-          [$LAST_MATCH_INFO.offset(0)[0] + n.length - 1, i]
-        ]
-      end
-      line.scan(SYM_REGEX) do |s|
-        syms << [s, [$LAST_MATCH_INFO.offset(0)[0], i]]
-      end
-    end
+  parts, syms = gather_parts(file, SYM_REGEX)
   parts.map do |part|
     part[0] if adjacent_symbol?(part, syms)
   end
@@ -48,23 +53,7 @@ def day03A(file)
 end
 
 def day03B(file)
-  parts = []
-  gears = []
-  File
-    .read(file)
-    .split("\n")
-    .each_with_index do |line, i|
-      line.scan(/\d+/) do |n|
-        parts << [
-          n.to_i,
-          [$LAST_MATCH_INFO.offset(0)[0], i],
-          [$LAST_MATCH_INFO.offset(0)[0] + n.length - 1, i]
-        ]
-      end
-      line.scan(GEAR_REGEX) do |s|
-        gears << [s, [$LAST_MATCH_INFO.offset(0)[0], i]]
-      end
-    end
+  parts, gears = gather_parts(file, GEAR_REGEX)
   gears.map do |gear|
     adjacents = gear_adjacent_parts(gear, parts)
     adjacents.length > 1 ? adjacents.inject(:*) : 0
