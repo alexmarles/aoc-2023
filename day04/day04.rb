63d595b44806e741a516dfdc01ee0d7c596a17f3
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-04 12:51:43 +0100

    Refactor Day04

--- day04/day04.rb
+++ day04/day04.rb
@@ -1,34 +1,29 @@
 # --- Day 4: Scratchcards ---
 
 def day04A(file)
-  File
-    .read(file)
-    .split("\n")
-    .map do |card|
-      numbers = card.split(': ')[1]
-      win, own = numbers.split(' | ').map { |list| list.split(' ') }
-      common = win & own
-      common.any? ? 2**(common.length - 1) : 0
-    end
-    .sum
+  File.read(file).split("\n").map do |card|
+    win, own = card
+               .split(': ')[1]
+               .split(' | ')
+               .map { |list| list.split(' ') }
+    common = win & own
+    common.any? ? 2**(common.length - 1) : 0
+  end
+  .sum
 end
 
 def day04B(file)
   cards = {}
-  File
-    .read(file)
-    .split("\n")
-    .each_with_index do |card, i|
-      cards[i] ? cards[i] += 1 : cards[i] = 1
-      numbers = card.split(': ')[1]
-      win, own = numbers.split(' | ').map { |list| list.split(' ') }
-      common = win & own
-      cards[i].times do
-        common.length.times do |j|
-          nci = i + j + 1
-          cards[nci] ? cards[nci] += 1 : cards[nci] = 1
-        end
-      end
+  File.read(file).split("\n").each_with_index do |card, i|
+    cards[i] = 1 unless cards[i]
+    win, own = card
+               .split(': ')[1]
+               .split(' | ')
+               .map { |list| list.split(' ') }
+    winning = (win & own).length
+    ((i + 1)..(i + winning)).each do |next_card|
+      cards[next_card] = (cards[next_card] || 1) + cards[i]
     end
+  end
   cards.values.sum
 end
