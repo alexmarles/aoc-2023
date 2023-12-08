2a958120eb84c9a353a034f91c52f88999596dab
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-08 13:22:24 +0100

    Refactor Day08

--- day08/day08.rb
+++ day08/day08.rb
@@ -1,6 +1,6 @@
 # --- Day 8: Haunted Wasteland ---
 
-def day08A(file)
+def parse_map(file)
   graph = {}
   steps, nodes = File.read(file).split("\n\n")
   steps = steps.split('').map(&:to_sym)
@@ -9,6 +9,12 @@ def day08A(file)
     sides = sides.gsub('(', '').gsub(')', '').split(', ')
     graph[label] = { L: sides[0], R: sides[1] }
   end
+  [steps, graph]
+end
+
+def day08A(file)
+  steps, graph = parse_map(file)
+
   i = 0
   current = 'AAA'
   while current != 'ZZZ'
@@ -19,25 +25,16 @@ def day08A(file)
 end
 
 def day08B(file)
-  graph = {}
-  steps, nodes = File.read(file).split("\n\n")
-  steps = steps.split('').map(&:to_sym)
-  nodes.split("\n").each do |n|
-    label, sides = n.split(' = ')
-    sides = sides.gsub('(', '').gsub(')', '').split(', ')
-    graph[label] = { L: sides[0], R: sides[1] }
-  end
-
-  currents = graph.keys.select { |p| p =~ /^.{2}A$/ }
-  finish = graph.keys.select { |p| p =~ /^.{2}Z$/ }
-
-  currents.map do |c|
-    i = 0
-    until finish.include? c
-      c = graph[c][steps[i % steps.length]]
-      i += 1
+  steps, graph = parse_map(file)
+  graph
+    .keys.select { |p| p =~ /^.{2}A$/ }
+    .map do |c|
+      i = 0
+      until c =~ /^.{2}Z$/
+        c = graph[c][steps[i % steps.length]]
+        i += 1
+      end
+      i
     end
-    i
-  end
-  .reduce(1, &:lcm)
+    .reduce(1, &:lcm)
 end
