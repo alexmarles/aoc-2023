91130bb396eb5ba8be560435beffd8d8135bf047
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-09 22:04:48 +0100

    Refactor Day09

--- day09/day09.rb
+++ day09/day09.rb
@@ -1,43 +1,34 @@
 # --- Day 9: Mirage Maintenance ---
 
+def history(line)
+  history = [line]
+  l = 1
+  loop do
+    history[l] = []
+    history[l - 1].zip(history[l - 1].drop(1) << 0) do |a, b|
+      history[l] << b - a
+    end
+    history[l].pop
+    l += 1
+    break if history[l - 1].map(&:zero?).all?
+  end
+  history
+end
+
 def day09A(file)
   File
     .read(file)
     .split("\n")
-    .map { |l| l.split.map(&:to_i) }
-    .map do |line|
-      history = [line]
-      l = 1
-      loop do
-        history[l] = []
-        history[l - 1].each_with_index do |n, i|
-          history[l] << history[l - 1][i + 1] - n unless history[l - 1][i + 1].nil?
-        end
-        l += 1
-        break if history[l - 1].map(&:zero?).all?
-      end
-      history.map(&:last).sum
-    end
-  .sum
+    .map { |l| history(l.split.map(&:to_i)) }
+    .map { |l| l.map(&:last).sum }
+    .sum
 end
 
 def day09B(file)
   File
     .read(file)
     .split("\n")
-    .map { |l| l.split.map(&:to_i) }
-    .map do |line|
-      history = [line]
-      l = 1
-      loop do
-        history[l] = []
-        history[l - 1].each_with_index do |n, i|
-          history[l] << history[l - 1][i + 1] - n unless history[l - 1][i + 1].nil?
-        end
-        l += 1
-        break if history[l - 1].map(&:zero?).all?
-      end
-      history.map(&:first).reverse.reduce { |acc, n| n - acc }
-    end
-  .sum
+    .map { |l| history(l.split.map(&:to_i)) }
+    .map { |l| l.map(&:first).reverse.reduce { |acc, n| n - acc } }
+    .sum
 end
