2147c64434125c6d02e025ee0783b93be9693b07
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-11 17:20:07 +0100

    Day 11: second puzzle

--- day11/day11.rb
+++ day11/day11.rb
@@ -1,6 +1,6 @@
 # --- Day 11: Cosmic Expansion ---
 
-def map_universe(file)
+def map_universe(file, expanse = 2)
   galaxies = []
   empty_rows = []
   empty_cols = []
@@ -8,7 +8,7 @@ def map_universe(file)
   universe
     .each_with_index do |row, r|
       if row.uniq == ['.']
-        empty_rows << (r + empty_rows.length) # Previous expanded rows offset
+        empty_rows << (r + empty_rows.length * (expanse - 1)) # Previous expanded rows offset
         next
       end
       row.each_with_index do |val, c|
@@ -21,37 +21,35 @@ def map_universe(file)
     .each_with_index do |col, c|
       next if col.uniq != ['.']
 
-      empty_cols << (c + empty_cols.length) # Previous expanded cols offset
+      empty_cols << (c + empty_cols.length * (expanse - 1)) # Previous expanded cols offset
     end
 
   empty_rows.each do |r|
-    galaxies.map! { |g| r < g[0] ? [g[0] + 1, g[1]] : g }
+    galaxies.map! { |g| r < g[0] ? [g[0] + (expanse - 1), g[1]] : g }
   end
 
   empty_cols.each do |c|
-    galaxies.map! { |g| c < g[1] ? [g[0], g[1] + 1] : g }
+    galaxies.map! { |g| c < g[1] ? [g[0], g[1] + (expanse - 1)] : g }
   end
 
   galaxies
 end
 
-def distance_between(pair)
+def manhattan(pair)
   a, b = pair
   (b[1] - a[1]).abs + (b[0] - a[0]).abs
 end
 
 def day11A(file)
-  map_universe(file)
-    .combination(2)
-    .to_a
-    .map do |pair|
-      distance_between pair
-    end
+  map_universe(file) # List of galaxies
+    .combination(2).to_a # Paris of galaxies
+    .map { |pair| manhattan pair } # Distance for each pair
     .sum
 end
 
-def day11B(file)
-  File
-    .read(file)
-    .split("\n")
+def day11B(file, expanse)
+  map_universe(file, expanse) # List of galaxies
+    .combination(2).to_a # Paris of galaxies
+    .map { |pair| manhattan(pair) } # Distance for each pair
+    .sum
 end
--- day11/day11.spec.rb
+++ day11/day11.spec.rb
@@ -12,13 +12,15 @@ class Testday11A < Test::Unit::TestCase
     assert_equal 9_370_588, result
   end
 
-  # def test_example_day11_B
-  #   result = day11B 'day11/test.txt'
-  #   assert_equal 2, result
-  # end
+  def test_example_day11_B
+    result = day11B 'day11/test.txt', 10
+    assert_equal 1030, result
+    result = day11B 'day11/test.txt', 100
+    assert_equal 8410, result
+  end
 
-  # def test_puzzle_day11_B
-  #   result = day11B 'day11/in.txt'
-  #   assert_equal 1005, result
-  # end
+  def test_puzzle_day11_B
+    result = day11B 'day11/in.txt', 1_000_000
+    assert_equal 746_207_878_188, result
+  end
 end
