58ce8cb3c9b44ba5a93e1e37abb3eb9f14de5ee6
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-20 22:19:31 +0100

    Day 19: second puzzle

--- day19/day19.rb
+++ day19/day19.rb
@@ -55,7 +55,68 @@ def day19A(file)
 end
 
 def day19B(file)
-  File
-    .read(file)
-    .split("\n")
+  flows = {}
+  workflows, = File
+               .read(file)
+               .split("\n\n")
+  workflows.split("\n").each do |wf|
+    label, rest = wf.split('{')
+    rules = rest[..-2].split(',')
+    conditions = [[], rules.slice!(-1)]
+    rules.each do |rule|
+      comparison, target = rule.split(':')
+      key = comparison[0]
+      cmp = comparison[1]
+      n = comparison[2..].to_i
+      conditions[0].push [key, cmp, n, target]
+    end
+    flows[label] = conditions
+  end
+
+  count = lambda do |ranges, name = 'in'| # rubocop:disable Metrics/BlockLength
+    return 0 if name == 'R'
+
+    if name == 'A'
+      product = 1
+      ranges.each_value do |lo, hi|
+        product *= hi - lo + 1
+      end
+      return product
+    end
+
+    rules, fallback = flows[name]
+
+    total = 0
+    broke = false
+
+    rules.each do |key, cmp, n, target|
+      lo, hi = ranges[key]
+      if cmp == '<'
+        t = [lo, n - 1]
+        f = [n, hi]
+      else
+        t = [n + 1, hi]
+        f = [lo, n]
+      end
+
+      if t[0] <= t[1]
+        copy = ranges.clone
+        copy[key] = t
+        total += count.call(copy, target)
+      end
+
+      if f[0] <= f[1]
+        ranges = ranges.clone
+        ranges[key] = f
+      else
+        broke = true
+        break
+      end
+    end
+    total += count.call(ranges, fallback) unless broke
+
+    total
+  end
+
+  count.call(Hash[%w[x m a s].map { |h| [h, [1, 4000]] }])
 end
--- day19/day19.spec.rb
+++ day19/day19.spec.rb
@@ -12,13 +12,13 @@ class TestDay19A < Test::Unit::TestCase
     assert_equal 489_392, result
   end
 
-  # def test_example_day19_B
-  #   result = day19B 'day19/test.txt'
-  #   assert_equal 952_408_144_115, result
-  # end
+  def test_example_day19_B
+    result = day19B 'day19/test.txt'
+    assert_equal 167_409_079_868_000, result
+  end
 
-  # def test_puzzle_day19_B
-  #   result = day19B 'day19/in.txt'
-  #   assert_equal 106_920_098_354_636, result
-  # end
+  def test_puzzle_day19_B
+    result = day19B 'day19/in.txt'
+    assert_equal 134_370_637_448_305, result
+  end
 end
