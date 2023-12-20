a644390aa81650ce608a97e953eeb3e81c34d906
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-20 01:42:24 +0100

    Day 18: second puzzle

--- day18/day18.rb
+++ day18/day18.rb
@@ -1,6 +1,6 @@
 # --- Day 18: Lavaduct Lagoon ---
 
-def day18A(file)
+def dig_a_trench(file, swap: false)
   dirs = { 'R': [0, 1], 'L': [0, -1], 'U': [-1, 0], 'D': [1, 0] }
   trench = Set.new [[0, 0]]
   r = 0
@@ -9,17 +9,26 @@ def day18A(file)
     .read(file)
     .split("\n")
     .map { |row| row.split(' ') }
-    .each do |dir, steps, _color|
-      dr, dc = dirs[dir.to_sym]
-      steps = steps.to_i
+    .each do |dir, steps, color|
+      if swap
+        hex = color.match(/\#([a-z0-9]{6})/)[1]
+        dr, dc = dirs['RDLU'[hex[-1].to_i].to_sym]
+        steps = hex[..-2].to_i 16
+      else
+        dr, dc = dirs[dir.to_sym]
+        steps = steps.to_i
+      end
       steps.times do
         r += dr
         c += dc
         trench.add [r, c]
       end
     end
+  trench
+end
 
-  trench = trench.to_a
+def day18A(file)
+  trench = dig_a_trench(file).to_a
   area = (0..trench.length - 1).map do |i|
     trench[i][0] * (trench[i - 1][1] - trench[(i + 1) % trench.length][1])
   end.sum.abs / 2
@@ -28,7 +37,10 @@ def day18A(file)
 end
 
 def day18B(file)
-  File
-    .read(file)
-    .split("\n")
+  trench = dig_a_trench(file, swap: true).to_a
+  area = (0..trench.length - 1).map do |i|
+    trench[i][0] * (trench[i - 1][1] - trench[(i + 1) % trench.length][1])
+  end.sum.abs / 2
+  boundary = trench.length
+  area - boundary / 2 + 1 + boundary
 end
--- day18/day18.spec.rb
+++ day18/day18.spec.rb
@@ -12,13 +12,13 @@ class TestDay18A < Test::Unit::TestCase
     assert_equal 40_761, result
   end
 
-  # def test_example_day18_B
-  #   result = day18B 'day18/test.txt'
-  #   assert_equal 51, result
-  # end
+  def test_example_day18_B
+    result = day18B 'day18/test.txt'
+    assert_equal 952_408_144_115, result
+  end
 
-  # def test_puzzle_day18_B
-  #   result = day18B 'day18/in.txt'
-  #   assert_equal 8143, result
-  # end
+  def test_puzzle_day18_B
+    result = day18B 'day18/in.txt'
+    assert_equal 106_920_098_354_636, result
+  end
 end
