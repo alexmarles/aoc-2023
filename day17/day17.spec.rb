6f396bab8778b4bd90b53e30e8dff93ec76403bd
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-20 03:09:39 +0100

    Day 17: second puzzle

--- day17/day17.spec.rb
+++ day17/day17.spec.rb
@@ -7,18 +7,21 @@ class TestDay17A < Test::Unit::TestCase
     assert_equal 102, result
   end
 
-  def test_puzzle_day17_A
-    result = day17A 'day17/in.txt'
-    assert_equal 755, result
-  end
-
-  # def test_example_day17_B
-  #   result = day17B 'day17/test.txt'
-  #   assert_equal 51, result
+  # def test_puzzle_day17_A
+  #   result = day17A 'day17/in.txt'
+  #   assert_equal 755, result # 22 minutes
   # end
+
+  def test_example_day17_B
+    result = day17B 'day17/test.txt'
+    assert_equal 94, result
+
+    result = day17B 'day17/test2.txt'
+    assert_equal 71, result
+  end
 
   # def test_puzzle_day17_B
   #   result = day17B 'day17/in.txt'
-  #   assert_equal 8143, result
+  #   assert_equal 881, result # 165 minutes
   # end
 end
