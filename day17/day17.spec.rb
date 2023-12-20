4fa6e16d980fc1cc4dc2c158fd2144a4542f9459
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-20 22:27:15 +0100

    Omit long tests

--- day17/day17.spec.rb
+++ day17/day17.spec.rb
@@ -7,10 +7,11 @@ class TestDay17A < Test::Unit::TestCase
     assert_equal 102, result
   end
 
-  # def test_puzzle_day17_A
-  #   result = day17A 'day17/in.txt'
-  #   assert_equal 755, result # 22 minutes
-  # end
+  def test_puzzle_day17_A
+    omit 'Takes 22 minutes'
+    result = day17A 'day17/in.txt'
+    assert_equal 755, result
+  end
 
   def test_example_day17_B
     result = day17B 'day17/test.txt'
@@ -20,8 +21,9 @@ class TestDay17A < Test::Unit::TestCase
     assert_equal 71, result
   end
 
-  # def test_puzzle_day17_B
-  #   result = day17B 'day17/in.txt'
-  #   assert_equal 881, result # 165 minutes
-  # end
+  def test_puzzle_day17_B
+    omit 'Takes 3 hours'
+    result = day17B 'day17/in.txt'
+    assert_equal 881, result
+  end
 end
