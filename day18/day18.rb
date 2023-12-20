165eb911225bdc021c01dac8dcc242a61138f520
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-20 01:49:19 +0100

    Refactor Day18

--- day18/day18.rb
+++ day18/day18.rb
@@ -2,9 +2,8 @@
 
 def dig_a_trench(file, swap: false)
   dirs = { 'R': [0, 1], 'L': [0, -1], 'U': [-1, 0], 'D': [1, 0] }
-  trench = Set.new [[0, 0]]
-  r = 0
-  c = 0
+  trench = [[0, 0]]
+  boundary = 0
   File
     .read(file)
     .split("\n")
@@ -18,29 +17,25 @@ def dig_a_trench(file, swap: false)
         dr, dc = dirs[dir.to_sym]
         steps = steps.to_i
       end
-      steps.times do
-        r += dr
-        c += dc
-        trench.add [r, c]
-      end
+      boundary += steps
+      r, c = trench[-1]
+      trench << [r + dr * steps, c + dc * steps]
     end
-  trench
+  [trench, boundary]
 end
 
 def day18A(file)
-  trench = dig_a_trench(file).to_a
+  trench, boundary = dig_a_trench(file).to_a
   area = (0..trench.length - 1).map do |i|
     trench[i][0] * (trench[i - 1][1] - trench[(i + 1) % trench.length][1])
   end.sum.abs / 2
-  boundary = trench.length
   area - boundary / 2 + 1 + boundary
 end
 
 def day18B(file)
-  trench = dig_a_trench(file, swap: true).to_a
+  trench, boundary = dig_a_trench(file, swap: true).to_a
   area = (0..trench.length - 1).map do |i|
     trench[i][0] * (trench[i - 1][1] - trench[(i + 1) % trench.length][1])
   end.sum.abs / 2
-  boundary = trench.length
   area - boundary / 2 + 1 + boundary
 end
