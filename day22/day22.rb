3ba7884e189215e58a3835c2f20e6c0d1d0e5422
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-23 00:40:01 +0100

    Day 22: second puzzle

--- day22/day22.rb
+++ day22/day22.rb
@@ -1,10 +1,10 @@
 # --- Day 22: Sand Slabs ---
 
-def day22A(file)
-  overlaps = lambda do |a, b|
-    [a[0][0], b[0][0]].max <= [a[1][0], b[1][0]].min && [a[0][1], b[0][1]].max <= [a[1][1], b[1][1]].min
-  end
+def overlaps(a, b)
+  [a[0][0], b[0][0]].max <= [a[1][0], b[1][0]].min && [a[0][1], b[0][1]].max <= [a[1][1], b[1][1]].min
+end
 
+def stack_bricks(file)
   bricks = File
            .read(file)
            .split("\n")
@@ -13,32 +13,57 @@ def day22A(file)
   bricks.each_with_index do |brick, index|
     max_z = 1
     bricks[...index].each do |check|
-      max_z = [max_z, check[1][2] + 1].max if overlaps.call(brick, check)
+      max_z = [max_z, check[1][2] + 1].max if overlaps(brick, check)
     end
     brick[1][2] -= brick[0][2] - max_z
     brick[0][2] = max_z
   end
   bricks.sort_by { |b| b[0][2] }
+end
 
-  a_supports_b = Hash[(0...bricks.length).to_a.map { |i| [i, Set.new] }]
-  b_supports_a = Hash[(0...bricks.length).to_a.map { |i| [i, Set.new] }]
+def get_supports(bricks)
+  supports = Hash[(0...bricks.length).to_a.map { |i| [i, Set.new] }]
+  supported_by = Hash[(0...bricks.length).to_a.map { |i| [i, Set.new] }]
 
   bricks.each_with_index do |upper, j|
     bricks[...j].each_with_index do |lower, i|
-      if overlaps.call(lower, upper) && upper[0][2] == lower[1][2] + 1
-        a_supports_b[i].add j
-        b_supports_a[j].add i
+      if overlaps(lower, upper) && upper[0][2] == lower[1][2] + 1
+        supports[i].add j
+        supported_by[j].add i
       end
     end
   end
+  [supports, supported_by]
+end
+
+def day22A(file)
+  bricks = stack_bricks(file)
+  supports, supported_by = get_supports(bricks)
 
   bricks.length.times.map do |i|
-    a_supports_b[i].map { |j| b_supports_a[j].length >= 2 }.all?
+    supports[i].map { |j| supported_by[j].length >= 2 }.all?
   end.select(&:itself).count
 end
 
 def day22B(file)
-  File
-    .read(file)
-    .split("\n")
+  bricks = stack_bricks(file)
+  supports, supported_by = get_supports(bricks)
+
+  fallen = []
+  bricks.length.times.map do |i|
+    queue = supports[i].select { |lower| supported_by[lower].length == 1 }
+    will_fall = queue.to_set
+
+    while queue.any?
+      upper = queue.shift
+      (supports[upper] - will_fall).each do |brick| # Set substraction
+        if supported_by[brick] <= will_fall # Subset of
+          will_fall.add brick
+          queue.push brick
+        end
+      end
+    end
+    fallen << will_fall.to_a
+  end
+  fallen.map(&:count).sum
 end
--- day22/day22.spec.rb
+++ day22/day22.spec.rb
@@ -12,13 +12,13 @@ class TestDay22A < Test::Unit::TestCase
     assert_equal 393, result
   end
 
-  # def test_example_day22_B
-  #   result = day22B 'day22/test.txt'
-  #   assert_equal 167_409_079_868_000, result
-  # end
+  def test_example_day22_B
+    result = day22B 'day22/test.txt'
+    assert_equal 7, result
+  end
 
-  # def test_puzzle_day22_B
-  #   result = day22B 'day22/in.txt'
-  #   assert_equal 134_370_637_448_305, result
-  # end
+  def test_puzzle_day22_B
+    result = day22B 'day22/in.txt'
+    assert_equal 58_440, result
+  end
 end
