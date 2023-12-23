01f1c1e0197c90452fd529ddf3a0fae40634c6ae
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-23 22:10:43 +0100

    Day 23: second puzzle

--- day23/day23.rb
+++ day23/day23.rb
@@ -1,42 +1,79 @@
 # --- Day 23: A Long Walk ---
 
+$dirs = {
+  '^': [[-1, 0]],
+  'v': [[1, 0]],
+  '<': [[0, -1]],
+  '>': [[0, 1]],
+  '.': [[-1, 0], [1, 0], [0, -1], [0, 1]]
+}
+
 def valid_position(grid, r, c)
   r >= 0 && r < grid.length &&
     c >= 0 && c < grid[r].length &&
-    grid[r][c] != '#' &&
-    grid[r][c] != 'E'
+    grid[r][c] != '#'
 end
 
-def dfs(grid, r, c, visited, trails)
-  return trails[[r, c]] || 0 if visited.include? [r, c]
+def collapse_grid(grid, sr, sc, er, ec)
+  points = [[sr, sc], [er, ec]]
+  0.upto(grid.length - 1).each do |r|
+    0.upto(grid[r].length - 1).each do |c|
+      next unless valid_position(grid, r, c)
 
-  visited.push [r, c]
-  max_cost = 1
+      adj = 0
+      $dirs[:'.'].each do |dr, dc|
+        nr = r + dr
+        nc = c + dc
+        next unless valid_position(grid, nr, nc)
 
-  [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]].each do |nr, nc|
-    next unless valid_position(grid, nr, nc)
-    next if visited.include?([nr, nc])
-
-    cost = 1
-    case grid[nr][nc]
-    when '>'
-      nc += 1
-      cost += 1
-    when '<'
-      nc -= 1
-      cost += 1
-    when 'v'
-      nr += 1
-      cost += 1
-    when '^'
-      nr -= 1
-      cost += 1
+        adj += 1
+      end
+      points.push([r, c]) if adj > 2
     end
-
-    path_cost = cost + dfs(grid, nr, nc, visited, trails)
-    max_cost = [max_cost, path_cost].max
   end
-  trails[[r, c]] = max_cost
+  graph = Hash[(points.map { |j| [j, {}] })]
+
+  points.each do |pr, pc|
+    stack = [[0, pr, pc]]
+    seen = [[pr, pc]].to_set
+
+    while stack.any?
+      n, r, c = stack.pop
+
+      if n != 0 && points.include?([r, c])
+        graph[[pr, pc]][[r, c]] = n
+        next
+      end
+
+      $dirs[grid[r][c].to_sym].each do |dr, dc|
+        nr = r + dr
+        nc = c + dc
+        next unless nr >= 0 && nr < grid.length
+        next unless nc >= 0 && nc < grid.length
+        next unless grid[nr][nc] != '#'
+        next if seen.include?([nr, nc])
+
+        stack.push [n + 1, nr, nc]
+        seen.add [nr, nc]
+      end
+    end
+  end
+  graph
+end
+
+def dfs(graph, node, end_node, visited)
+  return 0 if node == end_node
+
+  max_cost = -Float::INFINITY
+
+  visited.add node
+  graph[node].each do |adj, n|
+    unless visited.include? adj
+      trail_cost = n + dfs(graph, adj, end_node, visited)
+      max_cost = [max_cost, trail_cost].max
+    end
+  end
+  visited.delete node
   max_cost
 end
 
@@ -49,16 +86,21 @@ def day23A(file)
   sc = grid[sr].index('.')
   er = grid.length - 1
   ec = grid[er].index('.')
-  grid[sr][sc] = 'S'
-  grid[er][ec] = 'E'
 
-  trails = {}
-  dfs(grid, sr, sc, [], trails)
-  trails.values.max
+  graph = collapse_grid(grid, sr, sc, er, ec)
+  dfs(graph, [sr, sc], [er, ec], Set.new)
 end
 
 def day23B(file)
-  File
-    .read(file)
-    .split("\n")
+  grid = File
+         .read(file)
+         .split("\n")
+         .map { |l| l.gsub(/[\^v><]/, '.').split('') }
+  sr = 0
+  sc = grid[sr].index('.')
+  er = grid.length - 1
+  ec = grid[er].index('.')
+
+  graph = collapse_grid(grid, sr, sc, er, ec)
+  dfs(graph, [sr, sc], [er, ec], Set.new)
 end
--- day23/day23.spec.rb
+++ day23/day23.spec.rb
@@ -12,13 +12,13 @@ class TestDay23A < Test::Unit::TestCase
     assert_equal 2206, result
   end
 
-  # def test_example_day23_B
-  #   result = day23B 'day23/test.txt'
-  #   assert_equal 167_409_079_868_000, result
-  # end
+  def test_example_day23_B
+    result = day23B 'day23/test.txt'
+    assert_equal 154, result
+  end
 
-  # def test_puzzle_day23_B
-  #   result = day23B 'day23/in.txt'
-  #   assert_equal 134_370_637_448_305, result
-  # end
+  def test_puzzle_day23_B
+    result = day23B 'day23/in.txt'
+    assert_equal 6490, result
+  end
 end
