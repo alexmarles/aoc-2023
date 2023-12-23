7c85d9913ac029346b38b8397d9918e3f41641cf
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-23 01:06:19 +0100

    Day 21: second puzzle

--- day21/day21.rb
+++ day21/day21.rb
@@ -1,6 +1,6 @@
 # --- Day 21: Step Counter ---
 
-def day21A(file, steps)
+def get_grid(file)
   grid = File.read(file).split("\n").map { |l| l.split('') }
   sr = 0
   sc = 0
@@ -12,7 +12,10 @@ def day21A(file, steps)
       end
     end
   end
+  [grid, sr, sc]
+end
 
+def find_plots(grid, sr, sc, steps)
   plots = [].to_set
   seen = [[sr, sc]].to_set
   q = [[sr, sc, steps]]
@@ -23,10 +26,10 @@ def day21A(file, steps)
     plots.add [r, c] if s.even?
     next if s.zero?
 
-    [[r, c - 1], [r, c + 1], [r - 1, c], [r + 1, c]].each do |nr, nc|
-      next unless nr.positive? && nr < grid.length
+    [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]].each do |nr, nc|
+      next unless nr >= 0 && nr < grid.length
 
-      next unless nc.positive? && nc < grid[nr].length
+      next unless nc >= 0 && nc < grid[0].length
 
       next unless grid[nr][nc] != '#' && !seen.include?([nr, nc])
 
@@ -37,6 +40,49 @@ def day21A(file, steps)
   plots.length
 end
 
-def day21B(file)
-  File.read(file).split("\n")
+def day21A(file, steps)
+  find_plots(*get_grid(file), steps)
+end
+
+def day21B(file, steps)
+  grid, sr, sc = get_grid(file)
+
+  raise 'Grid is not square' unless grid.length == grid[0].length
+
+  size = grid.length
+
+  raise 'Start is not middle' unless sr == (size / 2) && sc == (size / 2)
+
+  raise 'This is wrong' unless (steps % size) == (size / 2)
+
+  grid_width = (steps / size) - 1
+
+  odd = (((grid_width / 2) * 2) + 1)**2
+  even = (((grid_width + 1) / 2) * 2)**2
+
+  odd_points = find_plots(grid, sr, sc, (size * 2) + 1)
+  even_points = find_plots(grid, sr, sc, size * 2)
+
+  corner_t = find_plots(grid, size - 1, sc, size - 1)
+  corner_r = find_plots(grid, sr, 0, size - 1)
+  corner_b = find_plots(grid, 0, sc, size - 1)
+  corner_l = find_plots(grid, sr, size - 1, size - 1)
+
+  small_tr = find_plots(grid, size - 1, 0, (size / 2) - 1)
+  small_tl = find_plots(grid, size - 1, size - 1, (size / 2) - 1)
+  small_br = find_plots(grid, 0, 0, (size / 2) - 1)
+  small_bl = find_plots(grid, 0, size - 1, (size / 2) - 1)
+
+  large_tr = find_plots(grid, size - 1, 0, ((size * 3) / 2) - 1)
+  large_tl = find_plots(grid, size - 1, size - 1, ((size * 3) / 2) - 1)
+  large_br = find_plots(grid, 0, 0, ((size * 3) / 2) - 1)
+  large_bl = find_plots(grid, 0, size - 1, ((size * 3) / 2) - 1)
+
+  [
+    odd * odd_points,
+    even * even_points,
+    corner_t + corner_r + corner_b + corner_l,
+    (grid_width + 1) * (small_tr + small_tl + small_br + small_bl),
+    grid_width * (large_tr + large_tl + large_br + large_bl)
+  ].sum
 end
--- day21/day21.spec.rb
+++ day21/day21.spec.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 class TestDay21A < Test::Unit::TestCase
   def test_example_day21_A
     result = day21A 'day21/test.txt', 6
-    assert_equal 15, result # Should be 16
+    assert_equal 16, result
   end
 
   def test_puzzle_day21_A
@@ -12,31 +12,8 @@ class TestDay21A < Test::Unit::TestCase
     assert_equal 3649, result
   end
 
-  # def test_example_day21_B
-  #   result = day21B 'day21/test.txt', 6
-  #   assert_equal 16, result
-
-  #   result = day21B 'day21/test.txt', 10
-  #   assert_equal 50, result
-
-  #   result = day21B 'day21/test.txt', 50
-  #   assert_equal 1594, result
-
-  #   result = day21B 'day21/test.txt', 100
-  #   assert_equal 6536, result
-
-  #   result = day21B 'day21/test.txt', 500
-  #   assert_equal 167_004, result
-
-  #   result = day21B 'day21/test.txt', 1000
-  #   assert_equal 668_697, result
-
-  #   result = day21B 'day21/test.txt', 5000
-  #   assert_equal 16_733_044, result
-  # end
-
-  # def test_puzzle_day21_B
-  #   result = day21B 'day21/in.txt', 26501365
-  #   assert_equal 233_338_595_643_977, result
-  # end
+  def test_puzzle_day21_B
+    result = day21B 'day21/in.txt', 26_501_365
+    assert_equal 612_941_134_797_232, result
+  end
 end
