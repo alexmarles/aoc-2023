5f44965d5b72517fda58306d5b08c5ff11efac47
Alex Marles Alcaraz <alex.marles@king.com>
2023-12-29 11:38:28 +0100

    Day 24: second puzzle

--- day24/day24.rb
+++ day24/day24.rb
@@ -1,5 +1,4 @@
 # --- Day 24: Never Tell Me The Odds ---
-
 class Hailstone
   attr_accessor :sx, :sy, :sz, :vx, :vy, :vz, :a, :b, :c
 
@@ -49,15 +48,39 @@ def day24A(file, min, max)
       next unless area.include?(x) && area.include?(y) # Not in the test area
       next unless [hs1, hs2].map { |hs| (x - hs.sx) * hs.vx >= 0 && (y - hs.sy) * hs.vy >= 0 }.all? # Not in the past
 
-      collisions.push [hs1, hs2]
+      collisions.push [hs1, hs2] # Hailstones intersect
     end
   end
   collisions.count
 end
 
 def day24B(file)
-  File
-    .read(file)
-    .split("\n")
-    .map { |l| l.split('') }
+  hails = File
+          .read(file)
+          .split("\n")
+          .map { |line| Hailstone.new(line.split(' @ ').map { |side| side.split(', ').map(&:to_i) }) }
+
+  # xh + t * vxh = xr + t * vxr
+  # ->
+  # t * vxh - t * vxr = xr - xh
+  # ->
+  # t = (xr - xh) / (vxh - vxr) = (yr - yh) / (vyh - vyr) = (zr - zh) / (vzh - vzr)
+  # ->
+  # (xr - h1sx) * (h1vy - vyr) = (yr - h1sy) * (h1vx - vxr)
+  # (yr - h1sy) * (h1vz - vzr) = (zr - h1sz) * (h1vy - vyr)
+  # (xr - h2sx) * (h2vy - vyr) = (yr - h2sy) * (h2vx - vxr)
+  # (yr - h2sy) * (h2vz - vzr) = (zr - h2sz) * (h2vy - vyr)
+  # (xr - h3sx) * (h3vy - vyr) = (yr - h3sy) * (h3vx - vxr)
+  # (yr - h3sy) * (h3vz - vzr) = (zr - h3sz) * (h3vy - vyr)
+
+  # Print system of equations and solve it using MATLAB
+  puts "\n"
+  puts 'syms xr yr zr vxr vyr vzr'
+  hails[0..2].each_with_index do |h, i|
+    puts "eq#{i * 2 + 1} = (xr - #{h.sx}) * (#{h.vy} - vyr) == (yr - #{h.sy}) * (#{h.vx} - vxr)"
+    puts "eq#{i * 2 + 2} = (yr - #{h.sy}) * (#{h.vz} - vzr) == (zr - #{h.sz}) * (#{h.vy} - vyr)"
+  end
+  puts 'sol = solve([eq1, eq2, eq3, eq4, eq5, eq6], [xr, yr, zr, vxr, vyr, vzr])'
+  puts 'sol.xr + sol.yr + sol.zr'
+  puts 'ans(1)'
 end
--- day24/day24.spec.rb
+++ day24/day24.spec.rb
@@ -12,13 +12,15 @@ class TestDay24A < Test::Unit::TestCase
     assert_equal 29_142, result
   end
 
-  # def test_example_day24_B
-  #   result = day24B 'day24/test.txt'
-  #   assert_equal 154, result
-  # end
+  def test_example_day24_B
+    omit 'Solved using MATLAB'
+    result = day24B 'day24/test.txt'
+    assert_equal 47, result
+  end
 
-  # def test_puzzle_day24_B
-  #   result = day24B 'day24/in.txt'
-  #   assert_equal 6490, result
-  # end
+  def test_puzzle_day24_B
+    omit 'Solved using MATLAB'
+    result = day24B 'day24/in.txt'
+    assert_equal 848_947_587_263_033, result
+  end
 end
